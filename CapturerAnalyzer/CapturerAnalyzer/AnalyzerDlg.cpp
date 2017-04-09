83e33fd71ce8aa1deb31a6ff529277520e88c105
jiangyt2112 <946692706@qq.com>
2017-04-09 22:33:31 +0800

    windows

---
+++ CapturerAnalyzer/CapturerAnalyzer/AnalyzerDlg.cpp
@@ -0,0 +1,46 @@
+// AnalyzerDlg.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "CapturerAnalyzer.h"
+#include "AnalyzerDlg.h"
+#include "afxdialogex.h"
+
+
+// CAnalyzerDlg 对话框
+
+IMPLEMENT_DYNAMIC(CAnalyzerDlg, CDialogEx)
+
+CAnalyzerDlg::CAnalyzerDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(CAnalyzerDlg::IDD, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDI_ANALYZER);
+}
+
+CAnalyzerDlg::~CAnalyzerDlg()
+{
+}
+
+void CAnalyzerDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CAnalyzerDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CAnalyzerDlg 消息处理程序
+
+
+BOOL CAnalyzerDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+	// 设置此对话框的图标。当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+}
---
+++ CapturerAnalyzer2/CapturerAnalyzer/AnalyzerDlg.h
@@ -0,0 +1,27 @@
+#pragma once
+
+
+// CAnalyzerDlg 对话框
+
+class CAnalyzerDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CAnalyzerDlg)
+
+public:
+	CAnalyzerDlg(CWnd* pParent = NULL);   // 标准构造函数
+	virtual ~CAnalyzerDlg();
+
+// 对话框数据
+	enum { IDD = IDD_ANALYZER };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+public:
+	virtual BOOL OnInitDialog();
+private:
+	HICON m_hIcon;
+public:
+	afx_msg void OnBnClickedOk();
+};
